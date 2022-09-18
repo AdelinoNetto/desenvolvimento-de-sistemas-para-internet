88e61a8e43bcfcb3178e82fb75a279eec39b6f73
Adelino <adelino.mecanica@gmail.com>
2022-09-18 15:07:01 -0300

    criado arquivo programa cotacao compra e venda dolar

---
+++ 2022-09-18/CotacaoDolar.java
@@ -0,0 +1,36 @@
+package sanatnder;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CotacaoDolar {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double[] numeros = {5.12, 4.80, 4.60, 5.20, 3.50, 5.00};
+		double maior = numeros[0];
+		double menor = numeros[0];
+		double lucro = 0.0;
+		
+		for(int i = 0; i < numeros.length; i++) {
+			if(numeros[i] > maior) {
+				maior = numeros[i];
+			} 
+			if(numeros[i] < menor) {
+				menor = numeros[i];
+			}
+		}
+		
+		System.out.println("Melhor valor pra compra: U$" + menor);
+		System.out.println("Melhor valor pra venda: U$" + maior);
+		lucro = (maior - menor);
+		System.out.printf("Lucro de: U$%.2f.", lucro);
+		
+		
+		sc.close();
+	}
+
+}
